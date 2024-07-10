3d6083fe22eb666e3efb3870360e98aedbdca965
Abdelrahman Saber <109411513+AbdelrahmanSaber74@users.noreply.github.com>
2024-07-10 22:32:25 +0300

    Add InsertionSortExample class with insertion sort implementation

---
+++ Sorting/InsertionSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class InsertionSortExample
+{
+    // Method for performing insertion sort
+    public void InsertionSort(int[] array)
+    {
+        for (int i = 1; i < array.Length; i++)
+        {
+            int key = array[i];
+            int j = i - 1;
+
+            // Move elements of array[0..i-1] that are greater than key
+            // to one position ahead of their current position
+            while (j >= 0 && array[j] > key)
+            {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
+
+    // Method for printing the array
+    public void PrintArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Sorting/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] array = { 5, 2, 4, 6, 1, 3 , 9 , 7 };
+
+        InsertionSortExample sorter = new InsertionSortExample();
+
+        Console.WriteLine("Array before sorting:");
+        sorter.PrintArray(array);
+
+        sorter.InsertionSort(array);
+
+        Console.WriteLine("Array after sorting:");
+        sorter.PrintArray(array);
+    }
+}
