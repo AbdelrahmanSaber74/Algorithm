004199c4b909ff0474945778c14fd132b9615030
Abdelrahman Saber <109411513+AbdelrahmanSaber74@users.noreply.github.com>
2024-07-12 02:35:15 +0300

    Implemented BubbleSort algorithm

---
+++ Sorting/BubbleSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class BubbleSort
+{
+    // Method for performing insertion sort
+    public void BubbleSortAlgorithm(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    // Swap temp and arr[j]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // Method for printing the array
+    public void PrintArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -4,12 +4,21 @@
     {
         int[] array = { 5, 2, 4, 6, 1, 3, 9, 7 };
 
+        BubbleSort bubbleSorter = new BubbleSort();
         InsertionSort insertionSorter = new InsertionSort();
         SelectionSort selectionSorter = new SelectionSort();
 
+        // Sorting with Bubble Sort
+        int[] arrayBubbleSort = (int[])array.Clone(); // Create a copy for bubble sort
+        Console.WriteLine("Array before bubble sort:");
+        bubbleSorter.PrintArray(arrayBubbleSort);
+        bubbleSorter.BubbleSortAlgorithm(arrayBubbleSort);
+        Console.WriteLine("Array after bubble sort:");
+        bubbleSorter.PrintArray(arrayBubbleSort);
+
         // Sorting with Insertion Sort
         int[] arrayInsertionSort = (int[])array.Clone(); // Create a copy for insertion sort
-        Console.WriteLine("Array before insertion sort:");
+        Console.WriteLine("\nArray before insertion sort:");
         insertionSorter.PrintArray(arrayInsertionSort);
         insertionSorter.InsertionSortAlgorithm(arrayInsertionSort);
         Console.WriteLine("Array after insertion sort:");
@@ -22,5 +31,6 @@
         selectionSorter.SelectionSortAlgorithm(arraySelectionSort);
         Console.WriteLine("Array after selection sort:");
         selectionSorter.PrintArray(arraySelectionSort);
+
     }
 }
